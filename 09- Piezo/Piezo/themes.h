9f5f5e5556f0486af10d3a9f6cacd226a46753c1
Amirparsa-Sal <Amirparsa.s751@gmail.com>
2022-01-26 12:22:23 +0330

    Add 09-Piezo files

---
+++ 09- Piezo/Piezo/themes.h
@@ -0,0 +1,98 @@
+// notes in the jingleBellsMelody:
+int jingleBellsMelody[] = {
+  NOTE_E5, NOTE_E5, NOTE_E5,
+  NOTE_E5, NOTE_E5, NOTE_E5,
+  NOTE_E5, NOTE_G5, NOTE_C5, NOTE_D5,
+  NOTE_E5,
+  NOTE_F5, NOTE_F5, NOTE_F5, NOTE_F5,
+  NOTE_F5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5,
+  NOTE_E5, NOTE_D5, NOTE_D5, NOTE_E5,
+  NOTE_D5, NOTE_G5
+};
+
+// NOTE_durations: 4 = quarter note, 8 = eighth note, etc.:
+int jingleBellsNoteDurations[] = {
+
+  4, 4, 2,
+  4, 4, 2,
+  4, 4, 3, 8,
+  1,
+  4, 4, 4, 4,
+  4, 4, 4, 8, 8,
+  4, 4, 4, 4,
+  2, 2
+};
+
+int odeToJoyMelody[] = {
+  NOTE_E5, NOTE_E5, NOTE_F5, NOTE_G5,
+  NOTE_G5, NOTE_F5, NOTE_E5, NOTE_D5,
+  NOTE_C5, NOTE_C5, NOTE_D5, NOTE_E5,
+  NOTE_E5, NOTE_D5, NOTE_D5,
+  NOTE_E5, NOTE_E5, NOTE_F5, NOTE_G5,
+  NOTE_G5, NOTE_F5, NOTE_E5, NOTE_D5,
+  NOTE_C5, NOTE_C5, NOTE_D5, NOTE_E5,
+  NOTE_D5, NOTE_C5, NOTE_C5,
+  NOTE_D5, NOTE_D5, NOTE_E5, NOTE_C5,
+  NOTE_D5, NOTE_E5, NOTE_F5, NOTE_E5, NOTE_C5,
+  NOTE_D5, NOTE_E5, NOTE_F5, NOTE_E5, NOTE_D5,
+  NOTE_C5, NOTE_D5,
+};
+
+int odeToJoyNoteDurations[] = {
+  4, 4, 4, 4,
+  4, 4, 4, 4,
+  4, 4, 4, 4,
+  3, 8, 2,
+  4, 4, 4, 4,
+  4, 4, 4, 4,
+  4, 4, 4, 4,
+  3, 8, 2,
+  4, 4, 4, 4,
+  4, 4, 4, 4, 4,
+  4, 4, 4, 4, 4,
+  4, 4
+};
+
+int underworldMelody[] = {
+  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+  NOTE_AS3, NOTE_AS4, 0,
+  0,
+  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+  NOTE_AS3, NOTE_AS4, 0,
+  0,
+  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+  NOTE_DS3, NOTE_DS4, 0,
+  0,
+  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+  NOTE_DS3, NOTE_DS4, 0,
+  0, NOTE_DS4, NOTE_CS4, NOTE_D4,
+  NOTE_CS4, NOTE_DS4,
+  NOTE_DS4, NOTE_GS3,
+  NOTE_G3, NOTE_CS4,
+  NOTE_C4, NOTE_FS4, NOTE_F4, NOTE_E3, NOTE_AS4, NOTE_A4,
+  NOTE_GS4, NOTE_DS4, NOTE_B3,
+  NOTE_AS3, NOTE_A3, NOTE_GS3,
+  0, 0, 0
+};
+//Underwolrd tempo
+int underworldNoteDurations[] = {
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  6, 18, 18, 18,
+  6, 6,
+  6, 6,
+  6, 6,
+  18, 18, 18, 18, 18, 18,
+  10, 10, 10,
+  10, 10, 10,
+  3, 3, 3
+};
---
+++ 09- Piezo/PiezoWithPot/PiezoWithPot.ino
@@ -0,0 +1,92 @@
+#include "pitches.h"
+#include "themes.h"
+#include <Keypad.h>
+
+#define PIEZO_PIN 10
+#define POT_PIN A0
+
+#define NOTE_BASE_TIME 1000
+
+const byte ROWS = 4; 
+const byte COLS = 4;
+
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+#define KEYPAD1 9
+#define KEYPAD2 8
+#define KEYPAD3 7
+#define KEYPAD4 6
+#define KEYPAD5 5
+#define KEYPAD6 4
+#define KEYPAD7 3
+#define KEYPAD8 2
+
+byte rowPins[ROWS] = {KEYPAD1, KEYPAD2, KEYPAD3, KEYPAD4}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {KEYPAD5, KEYPAD6, KEYPAD7, KEYPAD8}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+void play_music(int melody[], int note_duration[], int melody_length);
+
+void setup() {
+  pinMode(POT_PIN, INPUT);
+  // iterate over the notes of the jingleBellsMelody:
+}
+
+void loop() {
+  char c = keypad.waitForKey();
+  if (c == '1'){
+    play_music(jingleBellsMelody, jingleBellsNoteDurations, sizeof(jingleBellsMelody)/sizeof(int));
+  }
+  else if (c == '2'){
+    play_music(odeToJoyMelody, odeToJoyNoteDurations, sizeof(odeToJoyMelody)/sizeof(int));
+  }
+  else if (c == '3'){
+    play_music(underworldMelody, underworldNoteDurations, sizeof(underworldMelody)/sizeof(int));
+  }
+   no need to repeat the jingleBellsMelody.
+}
+
+void play_music(int melody[], int note_duration[], int melody_length){
+  
+  for (int thisNote= 0; thisNote< melody_length; thisNote++) {
+
+    // to calculate the NOTE_duration, take NOTE_BASE_TIME divided by the NOTE_type.
+
+    //e.g. quarter NOTE_= NOTE_BASE_TIME / 4, eighth NOTE_= NOTE_BASE_TIME/8, etc.
+
+    int noteDuration = NOTE_BASE_TIME / note_duration[thisNote];
+
+    // Calculating pot value
+    int pot = analogRead(POT_PIN);
+    int scale = map(pot, 0, 1023, 0, 2);
+
+    int note = melody[thisNote];
+    if (scale == 0){
+      note = note / 2;
+    }
+    else if (scale == 2){
+      note = note * 2;
+    }
+    
+    tone(PIEZO_PIN, note, noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+
+    // the note's duration + 30% seems to work well:
+
+    int pauseBetweenNotes = noteDuration * 1.30;
+
+    delay(pauseBetweenNotes);
+
+    // stop the tone playing:
+
+    noTone(PIEZO_PIN);
+  }
+  
+}
